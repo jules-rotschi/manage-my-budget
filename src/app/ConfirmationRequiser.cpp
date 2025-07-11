8795e09af43b747e46d8e2e2c633e22865cefbda
Jules Rotschi <rotschi.jules@gmail.com>
2025-07-11 23:33:27 +0200

    Fix confirm dialogs buttons width

--- src/app/ConfirmationRequiser.cpp
+++ src/app/ConfirmationRequiser.cpp
@@ -11,7 +11,9 @@ bool ConfirmAction(const std::string& message, const std::string& confirmButtonT
 	box.setText(QString::fromStdString(message));
 
 	QPushButton* confirmButton = box.addButton(QString::fromStdString(confirmButtonText), QMessageBox::AcceptRole);
+	confirmButton->setFixedWidth(150);
 	QPushButton* cancelButton = box.addButton("Annuler", QMessageBox::RejectRole);
+	cancelButton->setFixedWidth(150);
 	box.setDefaultButton(cancelButton);
 
 	box.exec();
